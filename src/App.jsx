222413e9503f654ae115a5f13f829fda521ddad4
harish-pasupuleti <harishpasupuleti18@gmail.com>
2025-10-29 00:06:15 +0530

    Add project files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import JSONTreeVisualizer from './components/JSONTreeVisualizer/JSONTreeVisualizer'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    
+     <JSONTreeVisualizer />
+    </>
+  )
+}
+
+export default App
