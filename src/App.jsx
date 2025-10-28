6ffc4badbaf6e932f4f79bda383cc141d3399bd5
harish-pasupuleti <harishpasupuleti18@gmail.com>
2025-10-29 00:21:37 +0530

    Created Files

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import JSONTreeVisualizer from './components/JSONTreeVisualizer/JSONTreeVisualizer'
+import JSONTreeVisualizer from './JSONTreeVisualizer'
 
 function App() {
   const [count, setCount] = useState(0)
